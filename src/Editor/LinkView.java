facec165c7f2a4573b30e29578ef4329a97cb99f
Justin Cotner <jdc9622@rit.edu>
2013-04-05 20:41:45 -0400

    LinkView completed

--- src/Editor/LinkView.java
+++ src/Editor/LinkView.java
@@ -1,6 +1,7 @@
 package Editor;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * This is the backend class for the Link View.
@@ -13,9 +14,13 @@ import java.util.Collections;
 public class LinkView {
 	private ArrayList<String> links = new ArrayList<String>();
 	private ArrayList<String> alphabeticalOrder = new ArrayList<String>();
-	
+
 	public LinkView(){}
 	
+	/**
+	 * Takes a string and finds all the URLs in it.
+	 * @param text
+	 */
 	public void findLinks(String text){
 		String[] temp = text.split(" ");
 		
@@ -27,6 +32,11 @@ public class LinkView {
 		}
 	}
 	
+	/**
+	 * 
+	 * @return String temp - a string representation of all the URLs in
+	 * the found by findLinks in the order they were found
+	 */
 	public String returnOrderofAppearance(){
 		String temp = "";
 		
@@ -37,17 +47,44 @@ public class LinkView {
 		return temp;
 	}
 	
+	/**
+	 * Sorts the URLs found by findLinks. Duplicates are not returned.
+	 * Instead each unique URL has a counter that shows how many times
+	 * it appears in the program.
+	 * 
+	 * @return String temp - a string representation of the URLs
+	 * in alphabetical order
+	 */
 	public String returnOrdered(){
 		String temp = "";
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		ArrayList<String> noDuplicates = new ArrayList<String>();
 		
 		Collections.sort(alphabeticalOrder);
 		
-		
-		
 		for(String s: alphabeticalOrder){
-			temp += s + "\n";
+			if(!noDuplicates.contains(s)){
+				noDuplicates.add(s);
+				noDuplicates.add("1");
+			}
+			
+			else{
+				int index = noDuplicates.indexOf(s);
+				int count = Integer.parseInt(noDuplicates.get(index + 1));
+				
+				count += 1;
+				
+				noDuplicates.set(index + 1, Integer.toString(count));
+			}
 		}
 		
+		for(int i = 0; i < noDuplicates.size() - 1; i++){
+			if(i % 2 == 0){
+				temp += noDuplicates.get(i) + ": " +
+					noDuplicates.get(i + 1) + "\n";
+			}
+		}
+
 		return temp;
 	}
 
--- src/Editor/LinkViewTest.java
+++ src/Editor/LinkViewTest.java
@@ -16,6 +16,8 @@ public class LinkViewTest extends TestCase{
 	private LinkView view = new LinkView();
 	
 	public void testreturnOrderOfAppearance(){
+		String testString = "http://www.google.com asdasod aosd " +
+				"www.aol.com aosudiosjad aosijd https://reddit.com";
 		view.findLinks(testString);
 		String temp = view.returnOrderofAppearance();
 		assertEquals("http://www.google.com\nwww.aol.com\n" +
@@ -23,10 +25,21 @@ public class LinkViewTest extends TestCase{
 	}
 	
 	public void testReturnOrdered(){
+		String testString = "http://www.google.com asdasod aosd " +
+				"www.aol.com aosudiosjad aosijd https://reddit.com";
 		view.findLinks(testString);
 		String temp = view.returnOrdered();
-		assertEquals("http://www.google.com\nhttps://reddit.com\nwww.aol.com\n",
+		assertEquals("http://www.google.com: 1\nhttps://reddit.com: 1" +
+				"\nwww.aol.com: 1\n",
 				temp);
+	}
+	
+	public void testDuplicatesOrdered(){
+		testString += " www.aol.com";
+		view.findLinks(testString);
+		String temp = view.returnOrdered();
+		assertEquals("http://www.google.com: 1\nhttps://reddit.com: 1" +
+				"\nwww.aol.com: 2\n", temp);
 	}
 
 }
